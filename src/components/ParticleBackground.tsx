c3e1259a17a6457ddb40761005fd6b8303ec4670
TanishqJangir <tanishqjangir001@gmail.com>
2026-03-25 23:54:50 +0530

    fixed an issue

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -1,8 +1,14 @@
-
+import { useRef } from 'react';
 
 const ParticlesBackground = () => {
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+
+
     return (
         <canvas
+            ref={canvasRef}
             className="fixed top-0 left-0 w-full h-full pointer-events-none z-0"
         >
 
