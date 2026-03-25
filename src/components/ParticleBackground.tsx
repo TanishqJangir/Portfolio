28c2e20a499b756f80cb3bf24164f722f4a11173
TanishqJangir <tanishqjangir001@gmail.com>
2026-03-25 17:05:31 +0530

    init all files

---
+++ src/components/ParticleBackground.tsx
@@ -0,0 +1,15 @@
+
+
+const ParticlesBackground = () => {
+    return (
+        <canvas
+            className="fixed top-0 left-0 w-full h-full pointer-events-none z-0"
+        >
+
+
+
+        </canvas>
+    );
+};
+
+export default ParticlesBackground;
