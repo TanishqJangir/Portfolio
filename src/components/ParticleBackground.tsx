aed02eb8c70a1725faa2cce81d62f69661a18346
TanishqJangir <tanishqjangir001@gmail.com>
2026-03-26 23:55:04 +0530

    init particles on the screen

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -1,10 +1,94 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 const ParticlesBackground = () => {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if(!canvas) return;
 
+        const ctx = canvas?.getContext('2d');
+        if(!ctx) return;
+
+
+        let particles: Particle[] = [];
+        const particleCount = 50;
+        const colors = ["rgba(255, 255, 255, 0.7)"];
+
+        class Particle{
+            x : number;
+            y : number;
+            radius : number;
+            color : string;
+            speedX : number;
+            speedY : number;
+
+            constructor(){
+                this.x = Math.random() * canvas!.width;
+                this.y = Math.random() * canvas!.height;
+                this.radius = Math.random() * 3 + 1;
+                this.color = colors[Math.floor(Math.random() * colors.length)];
+                this.speedX = (Math.random() - 0.5) * 0.5;
+                this.speedY = (Math.random() - 0.5) * 0.5;
+            }
+
+            draw(){
+                ctx!.beginPath();
+                ctx!.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+                ctx!.shadowBlur = 10;
+                ctx!.shadowColor = this.color;
+                ctx!.fillStyle = this.color;
+                ctx!.fill();
+            }
+
+            update(){
+                this.x += this.speedX;
+                this.y += this.speedY;
+
+
+
+                if(this.x < 0) this.x = canvas!.width;
+                if(this.x > canvas!.width) this.x = 0;
+                if(this.y < 0) this.y = canvas!.height;
+                if(this.y > canvas!.height) this.y = 0;
+
+                this.draw();
+            }
+        }
+
+
+        function createParticles(){
+            particles = [];
+            for(let i = 0; i< particleCount; i++){
+                particles.push(new Particle());
+            };
+        };
+
+        function handleResize(){
+            canvas!.width = window.innerWidth;
+            canvas!.height = window.innerHeight;
+            createParticles();
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        let animationId : number;
+        function animate(){
+            ctx!.clearRect(0, 0, canvas!.width, canvas!.height);
+            particles.forEach((p) => p.update());
+            animationId = requestAnimationFrame(animate);
+        }
+
+        animate();
+
+        return () => {
+            cancelAnimationFrame(animationId);
+            window.removeEventListener("resize", handleResize);
+        }
+  
+    }, [])
 
     return (
         <canvas
